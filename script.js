ae6435fa188000380506112406126b797cc6102b
Gildecy de Jesus Sá <101933897+gildecy@users.noreply.github.com>
2023-05-07 11:02:20 -0300

    Add files via upload
    simples formulario para agendar tarefas em tela

---
+++ script.js
@@ -0,0 +1,19 @@
+const form  = document.querySelector('form');
+const ul = document.querySelector('ul')
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+}
+const input = form.querySelector('input')
+const value = input.value
+
+const li = document.createElement('li')
+li.setAttribute('class', '"px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between' )
+li.textContent = value
+
+const button = document.createElement('button')
+button.setAttribute('class', 'text-red-700 delete')
+button.textContent = "x"
+
+li.appendChild(button)    
+ul.appendChild(li)
